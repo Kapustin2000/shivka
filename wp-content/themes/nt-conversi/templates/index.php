bb3f0040954f6d643497b0e4587ab043a9890afe
mikhail.kapustin@hys-enterprise.com <mikhail.kapustin@hys-enterprise.com>
2019-02-13 15:12:48 +0200

    Services

--- wp-content/themes/nt-conversi/templates/index.php
+++ wp-content/themes/nt-conversi/templates/index.php
@@ -7,6 +7,9 @@ Template Name: Home
 
 
 <?php
+$settings = pods('home_ui')->find();
+
+
 $params = array(
     'orderby'=>"order_weight.meta_value DESC,id DESC",
     'limit' => 1
@@ -50,7 +53,7 @@ $stock = pods('stock')->find();
                             <span class="underline">узнать больше</span>
                             <i class="icon icon-arrow"></i>
                         </div>
-
+                        <?php if($settings->field('main_screen')) { ?>
                         <div id="main-slider" class="main-slider">
                             <div class="decorative lavander"></div>
                             <div class="item">
@@ -63,13 +66,13 @@ $stock = pods('stock')->find();
                                 <div class="img" style="background-image: url(<?php bloginfo('template_url'); ?>/assets/img/Слой_1@1x.png);"></div>
                             </div>
                         </div>
-
-
-<!--                          <div class="video-carousel">-->
-<!--                              <div class="decorative lavander"></div>-->
-<!--                              <div class="video-item"></div>-->
-<!--                              <div class="video-progress"></div>-->
-<!--                          </div>-->
+                        <?php }else{ ?>
+                                                      <div class="video-carousel">
+                                                          <div class="decorative lavander"></div>
+                                                          <div class="video-item"></div>
+                                                          <div class="video-progress"></div>
+                                                      </div>
+                        <?php } ?>
                       </div>
                 </div>
             </div>
@@ -113,7 +116,7 @@ $stock = pods('stock')->find();
                     <div class="row">
                         <?php while($services->fetch()){ ?>
                             <div class="col-lg-4 col-12">
-                                <a href="<?=get_permalink($services->display('id'))?>" class="service-item">
+                                <a href="/services/category/?id=<?=$data->field('service_category')['slug']?>" class="service-item">
                                     <div class="service-img-wrap">
                                         <div class="service-img" style="background-image: url(<?=$services->display('preview')?>);"></div>
                                     </div>
