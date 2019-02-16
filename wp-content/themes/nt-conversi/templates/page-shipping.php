4bc127324f510acec3999fdc1c25b4de568fa504
Kapustin2000 <KapustinMM@mail.ru>
2019-02-16 19:12:18 +0200

    Deliver

--- wp-content/themes/nt-conversi/templates/page-shipping.php
+++ wp-content/themes/nt-conversi/templates/page-shipping.php
@@ -2,7 +2,7 @@
 /*
 Template Name: Shipping
 */
-
+$settings = pods('about_ui')->find();
 ?>
 
 <?php get_header(); ?>
@@ -17,23 +17,15 @@ Template Name: Shipping
                             <span>&nbsp;доставка и&nbsp;<br>&nbsp;оплата&nbsp;</span>
                         </h1>
                         <h2>
-                            Доставка готовых изделий
-                            может осуществляться
-                            несколькими способами:
+                           <?=$settings->display('first_block_title')?>
                         </h2>
                         <div class="content-editable">
-                            <ul>
-                                <li>Междугородние службы доставки: Новая Почта, Ин Тайм, Автолюкс, Гюнсел. Стоимость доставки, в этом случае, будет  зависеть от габаритов и веса посылки.</li>
-                                <li>Доставка курьером по Киеву от 70 грн.</li>
-                                <li>Бесплатная доставка по Киеву при заказе от 10.000 грн.</li>
-                                <li>Самовывоз по Киеву и Белой Церкви.  Адреса, по которым  вы можете забрать готовые изделия, указаны в разделе
-                                    <a href="/contacts">Контакты</a> .</li>
-                            </ul>
+                            <?=$settings->display('first_block_text')?>
                         </div>
                     </div>
                 </div>
                 <div class="delivery-img-wrap">
-                    <img src="<?php bloginfo('template_url'); ?>/assets/img/delivery1.png" alt="Delivery">
+                    <img src=" <?=$settings->display('first_block_image')?>" alt="<?=$settings->field('first_block_image')['post_title']?>">
                 </div>
             </div>
         </div>
@@ -43,17 +35,12 @@ Template Name: Shipping
         <div class="container">
             <div class="row">
                 <div class="payment-img-wrap col-7">
-                    <img src="<?php bloginfo('template_url'); ?>/assets/img/delivery2.png" alt="Delivery">
+                    <img src="<?=$settings->display('second_block_image')?>" alt="<?=$settings->field('second_block_image')['post_title']?>">
                 </div>
                 <div class="payment-info col-5">
-                    <h2>способы оплаты заказа:</h2>
+                    <h2> <?=$settings->display('second_block_title')?></h2>
                     <div class="content-editable">
-                        <ul>
-                            <li>Онлайн оплата через системы платежей Liqpay, Приват24.</li>
-                            <li>Наличкой курьеру (по Киеву).</li>
-                            <li>При получении заказа на отделении новой почты — наложенный платеж. При этой оплате, заказчик платит за обратный возврат денег продавцу.</li>
-                            <li>На карту приват через терминалы пополнения (i-box).</li>
-                        </ul>
+                        <?=$settings->display('second_block_text')?>
                     </div>
                 </div>
             </div>
