09292525e2f3d1e26ccfca2a92e909a6c88d6219
mikhail.kapustin@hys-enterprise.com <mikhail.kapustin@hys-enterprise.com>
2019-02-28 15:48:37 +0200

    Gallery page

--- wp-content/themes/nt-conversi/templates/works-gallery.php
+++ wp-content/themes/nt-conversi/templates/works-gallery.php
@@ -2,9 +2,41 @@
 /*
 Template Name: Works Gallery
 */
+if(isset($_GET['id'])) {
+    $id = shivka_escapeParam(trim($_GET['id'], '/'));
+    $params = array('limit' => 1,
+        'where' => "post_name = '" . $id . "'"
+    );
+    $data = pods('gallery')->find($params);
+
+    $found = false;
+    if (!empty($data->total_found())) {
+        while ($data->fetch()) {
+            shivka_SetPodsSeo($data->display('id'));
+            $found = true;
+            break;
+        }
+    }
+}else{
+    $found = false;
+}
 
 ?>
 <?php get_header(); ?>
+
+<?php if($found) {  ?>
+
+
+
+
+
+<?php }else{ ?>
+
+    <script language="javascript" type="text/javascript">
+        document.location = "/404";
+    </script>
+
+<?php } ?>
 
 <?php get_footer(); ?>
 
