c8bbe2e7c84f5cacc30895aef6e6c063a97c92f5
Maria Zakharova <maria.zakharova@hys-enterprise.com>
2019-02-28 16:11:59 +0200

    works gallery

--- wp-content/themes/nt-conversi/templates/works-gallery.php
+++ wp-content/themes/nt-conversi/templates/works-gallery.php
@@ -27,7 +27,69 @@ if(isset($_GET['id'])) {
 <?php if($found) {  ?>
 
 
-
+    <div class="smarthoop-wrap smarthoop-works">
+        <section class="work-info">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-12">
+                        <div class="work-info-img">
+                            <img src="<?=$data->display('image')?>" alt="Work">
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-12">
+                        <div class="info-block">
+                            <h1><span>&nbsp;<?=$data->display('post_title')?>&nbsp;</span></h1>
+                            <?=$data->display('post_content')?>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="work-gallery">
+                            <div class="gallery-item">
+                                <img src="<?=$data->display('image')?>" alt="Work">
+                            </div>
+                            <div class="gallery-item">
+                                <img src="<?=$data->display('image')?>" alt="Work">
+                            </div>
+                            <div class="gallery-item">
+                                <img src="<?=$data->display('image')?>" alt="Work">
+                            </div>
+                            <div class="gallery-item">
+                                <img src="<?=$data->display('image')?>" alt="Work">
+                            </div>
+                            <div class="gallery-item">
+                                <img src="<?=$data->display('image')?>" alt="Work">
+                            </div>
+                        </div>
+                        <div class="work-gallery-nav">
+                            <div class="gallery-nav-item">
+                                <img src="<?=$data->display('image')?>" alt="Work">
+                            </div>
+                            <div class="gallery-nav-item">
+                                <img src="<?=$data->display('image')?>" alt="Work">
+                            </div>
+                            <div class="gallery-nav-item">
+                                <img src="<?=$data->display('image')?>" alt="Work">
+                            </div>
+                            <div class="gallery-nav-item">
+                                <img src="<?=$data->display('image')?>" alt="Work">
+                            </div>
+                            <div class="gallery-nav-item">
+                                <img src="<?=$data->display('image')?>" alt="Work">
+                            </div>
+                        </div>
+                        <div class="bottom">
+                            <a href="/works" class="back">
+                            <span>
+                                <i class="icon icon-arrow"></i>
+                                Назад к работам
+                            </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 
 
 <?php }else{ ?>
