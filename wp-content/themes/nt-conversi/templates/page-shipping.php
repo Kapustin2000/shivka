38972e76b1a31bea7c680ab74f8a358138feb254
Maria Zakharova <maria.zakharova@hys-enterprise.com>
2019-04-10 09:57:56 +0300

    shipping

--- wp-content/themes/nt-conversi/templates/page-shipping.php
+++ wp-content/themes/nt-conversi/templates/page-shipping.php
@@ -11,21 +11,23 @@ $settings = pods('delivery_ui')->find();
     <section class="delivery">
         <div class="container">
             <div class="row">
-                <div class="delivery-info col-7">
-                    <div class="bordered">
-                        <h1>
-                            <span>&nbsp;доставка и&nbsp;<br>&nbsp;оплата&nbsp;</span>
-                        </h1>
-                        <h2>
-                           <?=$settings->display('first_block_title')?>
-                        </h2>
-                        <div class="content-editable">
-                            <?=$settings->display('first_block_text')?>
+                <div class="col-12">
+                    <div class="delivery-info">
+                        <div class="bordered">
+                            <h1>
+                                <span>&nbsp;доставка и&nbsp;<br>&nbsp;оплата&nbsp;</span>
+                            </h1>
+                            <h2>
+                                <?=$settings->display('first_block_title')?>
+                            </h2>
+                            <div class="content-editable">
+                                <?=$settings->display('first_block_text')?>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="delivery-img-wrap">
-                    <img src=" <?=$settings->display('first_block_image')?>" alt="<?=$settings->field('first_block_image')['post_title']?>">
+                    <div class="delivery-img-wrap">
+                        <img src=" <?=$settings->display('first_block_image')?>" alt="<?=$settings->field('first_block_image')['post_title']?>">
+                    </div>
                 </div>
             </div>
         </div>
@@ -34,13 +36,15 @@ $settings = pods('delivery_ui')->find();
         <div class="decorative light-yellow"></div>
         <div class="container">
             <div class="row">
-                <div class="payment-img-wrap col-7">
-                    <img src="<?=$settings->display('second_block_image')?>" alt="<?=$settings->field('second_block_image')['post_title']?>">
-                </div>
-                <div class="payment-info col-5">
-                    <h2> <?=$settings->display('second_block_title')?></h2>
-                    <div class="content-editable">
-                        <?=$settings->display('second_block_text')?>
+                <div class="col-12">
+                    <div class="payment-img-wrap">
+                        <img src="<?=$settings->display('second_block_image')?>" alt="<?=$settings->field('second_block_image')['post_title']?>">
+                    </div>
+                    <div class="payment-info">
+                        <h2> <?=$settings->display('second_block_title')?></h2>
+                        <div class="content-editable">
+                            <?=$settings->display('second_block_text')?>
+                        </div>
                     </div>
                 </div>
             </div>
