4fef214032ce5e0acd664eb2b06d02b29bc0128d
Kapustin2000 <KapustinMM@mail.ru>
2019-04-13 14:41:24 +0300

    How to order

--- wp-content/themes/nt-conversi/templates/how-to-order.php
+++ wp-content/themes/nt-conversi/templates/how-to-order.php
@@ -21,26 +21,46 @@ $stages_of_work = pods('stages_of_work')->find($params);
                         <span>как заказать</span>
                     </h1>
                     <div class="steps-wrap row">
-                        <?php $count = 0; while($stages_of_work->fetch()){ ?>
-                            <?php if($count!=0){?>
-<!--                                <div class="step content-editable col-xl-7 col-12">-->
-                                <div class="step content-editable col-7">
-                                    <?php if($stages_of_work->field('image')){ ?><img src="<?=$stages_of_work->display('image')?>" alt="<?=$stages_of_work->field('image')['post_title']?>"> <?php } ?>
-                                    <h3><?=$stages_of_work->display('post_title')?></h3>
-                                    <?=$stages_of_work->display('full_description')?>
-                                          </div>
-                            <?php }else{ ?>
-<!--                                <div class="step col-xl-7 col-12">-->
-                                <div class="step col-7">
-                                    <h3><?=$stages_of_work->display('post_title')?></h3>
-                                    <div class="content-editable">
-                                        <div class="content-editable">
-                                            <?=$stages_of_work->display('full_description')?>
-                                        </div>
-                                    </div>
+<!--                        --><?php //$count = 0; while($stages_of_work->fetch()){ ?>
+<!--                            --><?php //if($count!=0){?>
+<!--<!--                                <div class="step content-editable col-xl-7 col-12">-->-->
+<!--                                <div class="step content-editable col-7">-->
+<!--                                    --><?php //if($stages_of_work->field('image')){ ?><!--<img src="--><?//=$stages_of_work->display('image')?><!--" alt="--><?//=$stages_of_work->field('image')['post_title']?><!--"> --><?php //} ?>
+<!--                                    <h3>--><?//=$stages_of_work->display('post_title')?><!--</h3>-->
+<!--                                    --><?//=$stages_of_work->display('full_description')?>
+<!--                                          </div>-->
+<!--                            --><?php //}else{ ?>
+<!--<!--                                <div class="step col-xl-7 col-12">-->-->
+<!--                                <div class="step col-7">-->
+<!--                                    <h3>--><?//=$stages_of_work->display('post_title')?><!--</h3>-->
+<!--                                    <div class="content-editable">-->
+<!--                                        <div class="content-editable">-->
+<!--                                            --><?//=$stages_of_work->display('full_description')?>
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!---->
+<!--                                </div>-->
+<!--                            --><?php //}  $count++;} ?>
 
-                                </div>
-                            <?php }  $count++;} ?>
+
+
+
+                        <?php $count=0; while($stages_of_work->fetch()){ ?>
+                            <!--  Block 1 -->
+                            <?php if($count==0){ ?>
+
+
+
+
+                                <!--  Block 2 -->
+                            <?php $count++; }else{ ?>
+
+
+
+
+                            <?php $count = 0;} ?>
+
+                        <?php }   ?>
                     </div>
                 </div>
             </div>
