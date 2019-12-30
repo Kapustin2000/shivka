c2df333f8b6188d835901650d707be1ff9633f7c
Mikhail Kapustin <mikhail.kapustin@hys-enterprise.com>
2019-12-30 18:04:26 +0200

    Category

--- wp-content/themes/nt-conversi/templates/works-category.php
+++ wp-content/themes/nt-conversi/templates/works-category.php
@@ -14,6 +14,7 @@ if($id = shivka_escapeParam(basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_
         while ($data->fetch()) {
             shivka_SetPodsSeo($data->display('term_id',true));
             $found = true;
+            $related_gallery = pods('gallery')->find(array('where' => 'works.slug = "'.$id.'"'));
             break;
         }
     }
@@ -44,12 +45,19 @@ if($id = shivka_escapeParam(basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_
 
 
                         <div class="related-categories services extra-services">
-                            <div class="row ajax-call"></div>
-                        </div>
-                        <div class="see-more eye-hover" id="works-category-ajax" data-total="<?=count($data->field('related_works'))?>">
-                            <span class="underline">смотреть больше</span>
-                            <i class="icon icon-eye"></i>
-                        </div>
+                            <?php if($related_gallery->total_found()){  while($related_gallery->fetch()){?>
+                            <div class="col-xl-4 col-sm-6 col-12">
+                                <a href="<?=get_permalink($related_gallery->display('term_id'))?>" class="service-item">
+                                    <div class="service-img-wrap">
+                                        <div class="service-img" style="background-image: url(<?=$related_gallery->display('preview')?>);"></div>
+                                    </div>
+                                    <div class="service-title"><?=$related_gallery->display('name')?></div>
+                                    <span class="btn btn-primary btn-span">смотреть</span>
+                                </a>
+                            </div>
+                            <?php } } ?>
+                         </div>
+                        
                         <a href="/works" class="back">
                             <span>
                                 <i class="icon icon-arrow"></i>
